ace87ee5da33d71218e0237df588b336eadfff58
HighFlyer22 <idrissouamer@gmail.com>
2023-03-17 17:31:38 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main -  program generates and prints all possible distinct combinations of 2 digits
+ * main - program that prints all possible different combinations of 2 digits
  * Return: 0
  */
 int main(void)
